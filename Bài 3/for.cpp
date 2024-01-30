9534bc0f5a2cd10bc5f9d06274005bc0a6b8bc8d
Quach Cam Nguyen <nguyenb1910112@student.ctu.edu.vn>
2024-01-30 11:52:45 +0700

    for.cpp

--- Bài 3/for.cpp
+++ Bài 3/for.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     
-    for (int i = 1, i < 11, i ++) {
-        printf("vòng lặp for i: %d\n", i);
+    for (int i = 1, i < 11, i ++){
+        printf("Vòng lặp for i: %d\n",i);
     }
-
     return 0;
 }
